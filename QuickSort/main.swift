8e7d4582100f64163eb74c8b157e07987f6238d9
kelbinmdavid@gmail.com <kelbinmdavid@gmail.com>
2022-03-10 12:12:00 -0800

    Quick Sort Swift

---
+++ QuickSort/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  QuickSort
+//
+//  Created by Kelbin David on 2022-03-10.
+//
+
+import Foundation
+
+var arr = [5,4,7,3,9,1,4,6,9]
+quickSort(&arr, 0, arr.count - 1)
+print(arr)
